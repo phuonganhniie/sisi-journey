ad397d2f336b224d35cc2c03c81bb06d7e91654b
Anh <anh.do@stacktech.org>
2024-04-02 11:41:00 +0700

    refactor: using axios instead fetch in wakatime proxy

--- api/wakatime-proxy.js
+++ api/wakatime-proxy.js
@@ -1,55 +1,35 @@
+import axios from 'axios';
+
 export default async function handler(req) {
   const start = Date.now();
+
+  console.log("Request URL is:", req.url)
 
   const path = req.url.split("?")[0].replace("/api/wakatime-proxy/", "");
   const baseUrl = `https://wakatime.com/api/v1/users/current/${path}`;
   const apiKey = process.env.VITE_WAKATIME_API_KEY;
 
-  const url = new URL(baseUrl);
-  url.search = new URLSearchParams({
+  const params = new URLSearchParams({
     ...Object.fromEntries(new URL(req.url, "http://localhost").searchParams),
     api_key: apiKey,
   });
-  console.log("Calling Wakatime API URL:", url.toString());
+  const fullUrl = `${baseUrl}?${params}`;
+  console.log("Calling Wakatime API URL:", fullUrl);
 
   try {
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), 10000);
-
-    const wakatimeResponse = await fetch(url.toString(), {
-      method: req.method,
+    const response = await axios.get(fullUrl, {
       headers: {
         "Content-Type": "application/json",
       },
-      signal: controller.signal,
-    });
+      timeout: 10000,
+    })
 
-    clearTimeout(timeoutId);
-
-    if (!wakatimeResponse.ok) {
-      console.error(
-        "Wakatime API request failed:",
-        wakatimeResponse.statusText
-      );
-      return new Response(
-        JSON.stringify({ error: wakatimeResponse.statusText }),
-        {
-          status: wakatimeResponse.status,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    }
-
-    const body = await wakatimeResponse.json();
-    console.log("Response from Wakatime API:", body);
+    console.log("Response from Wakatime API:", response.data);
 
     console.log(`Time elapsed: ${Date.now() - start} ms`);
-
-    return new Response(JSON.stringify(body), {
-      status: wakatimeResponse.status,
+    
+    return new Response(JSON.stringify(response.data), {
+      status: response.status,
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json",
@@ -57,8 +37,12 @@ export default async function handler(req) {
     });
   } catch (error) {
     console.error("Error fetching from Wakatime API:", error);
-    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-      status: 500,
+
+    const status = error.response ? error.response.status : 500;
+    const statusText = error.response ? error.response.statusText : "Internal Server Error";
+
+    return new Response(JSON.stringify({ error: statusText }), {
+      status: status,
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json",
